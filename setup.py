a77a4529f6031c6cd39967993ce63f2f90bcd7c6
James Cuzella <james.cuzella@lyraphase.com>
2019-12-27 21:02:29 -0700

    setup.py: Protect setup() with __main__ check to avoid any re-exec upon re-import

--- setup.py
+++ setup.py
@@ -12,16 +12,16 @@ with open('README.md') as f:
 with open('LICENSE') as f:
     LICENSE = f.read()
 
-
-setup(
-    name='sample',
-    version='0.1.0',
-    description='Code (regular expresssions and NTLK) to tokenise (remove)' + \
-                ' Private Personal Information (PPI) in Python.',
-    long_description=README,
-    author='Lindsay Smith',
-    author_email='wapdat@gmail.com',
-    url='https://github.com/wapdat/ppi-sanitise',
-    license=LICENSE,
-    packages=find_packages(exclude=('tests', 'docs'))
-)
+if __name__ == '__main__':
+    setup(
+        name='sample',
+        version='0.1.0',
+        description='Code (regular expresssions and NTLK) to tokenise (remove)' + \
+                    ' Private Personal Information (PPI) in Python.',
+        long_description=README,
+        author='Lindsay Smith',
+        author_email='wapdat@gmail.com',
+        url='https://github.com/wapdat/ppi-sanitise',
+        license=LICENSE,
+        packages=find_packages(exclude=('tests', 'docs')),
+    )
