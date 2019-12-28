e92a0471a0518517ea2fc3599b8bf19da90ad54d
James Cuzella <james.cuzella@lyraphase.com>
2019-12-27 21:01:00 -0700

    setup.py: Fix pylint warnings

--- setup.py
+++ setup.py
@@ -7,19 +7,21 @@ from setuptools import setup, find_packages
 
 
 with open('README.md') as f:
-    readme = f.read()
+    README = f.read()
 
 with open('LICENSE') as f:
-    license = f.read()
+    LICENSE = f.read()
+
 
 setup(
     name='sample',
     version='0.1.0',
-    description='Code (regular expresssions and NTLK) to tokenise (remove) Private Personal Information (PPI) in Python.',
-    long_description=readme,
+    description='Code (regular expresssions and NTLK) to tokenise (remove)' + \
+                ' Private Personal Information (PPI) in Python.',
+    long_description=README,
     author='Lindsay Smith',
     author_email='wapdat@gmail.com',
     url='https://github.com/wapdat/ppi-sanitise',
-    license=license,
+    license=LICENSE,
     packages=find_packages(exclude=('tests', 'docs'))
 )
